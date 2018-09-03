4989f1c28e1e584603fb5d2f85e3b05b7c3f2746
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2018-09-03 22:27:15 +0800

    Update c3s.js

--- c3s.js
+++ c3s.js
@@ -4,6 +4,7 @@ const LABEL = FULL_LABEL.slice(0,LABEL_LEN);
 const PREFIX_LEN = 10 + LABEL_LEN;
 const PREFIX_BASE = 36;
 
+import {resetRules} from './resetRules.js';
 import {T} from '../jtype-system/t.js';
 
 T.defCollection("Prefix", {
@@ -15,9 +16,12 @@ let counter = 1;
 
 export default c3s;
 
-Object.assign(c3s,{});
+Object.assign(c3s,{scope,rescope});
 
-function c3s() {
+function c3s(parts, ...values) {
+  // not sure what this does yet
+  // but as an object c3s exports scope and rescope to allow components
+  // to request stylesheets be scoped to them
 }
 
 export function generateUniquePrefix() {
@@ -31,4 +35,70 @@ export function extendPrefix({prefix:existingPrefix}) {
   T.guard(T`Prefix`, existingPrefix);
   existingPrefix.push(generateUniquePrefix().prefix[0]);
 }
+
+export function findStyleSheet(url) {
+  return [...document.styleSheets].find(({href}) => href == url);
+}
+
+export function isStyleSheetAccessible(ss) {
+  try {
+    console.log(ss.cssRules);
+    return true;
+  } catch(e) {
+    return false;
+  }
+}
+
+// it may actually be better to clone the sheet using
+// a style element rather than cloning using the link 
+// which may both rely on and recause a network request
+export function cloneStyleSheet(ss) {
+  const newNode = ss.ownerNode.cloneNode(true);
+  document.head.insertAdjacentElement(newNode,'beforeEnd');
+  return newNode;
+}
+
+export function addResetRules(ss) {
+  resetRules.forEach(rule => {
+    ss.insertRule(rule);
+  });
+}
+
+// combinator can also be empty string ALL rules are to apply to component container
+// but generally this is no. 
+export function prefixAllrules(ss, prefix, combinator = ' ') {
+  [...ss.cssRules].forEach(rule => {
+    const selectors = rule.selectorText.split(/,/g);
+    const prefixedSelectors = selectors.map(sel => `${prefix}${combinator}${sel}`);
+    rule.selectorText = prefixedSelectors.join(', ');
+  });
+}
+
+export function scopeStylesheet(url,prefix,combinator = ' ') {
+  const ss = findStyleSheet(url);
+  if ( !isStyleSheetAccessible(ss) ) {
+    throw new TypeError(`Only CORS stylesheets can be scoped, because cross-origin rules cannot be accessed.`);
+  }
+  const scopedSS = cloneStyleSheet(ss);
+  addResetRules(scopedSS);
+  prefixAllrules(scopedSS,prefix, combinator);
+  return scopedSS;
+}
+
+export function scope(url) {
+  const prefix = '.' + generateUniquePrefix().prefix[0];
+  return {scopedSheet: scopeStyleSheet(url,prefix), prefix};
+}
+
+// used when the first scoping didn't work and we need to add more prefix to increase specificity
+// if this ever occurs
+// which is why we use '' combinator to add to the prefix of the already scoped sheet
+export function rescope({scopedSheet, prefix:existingPrefix}) {
+  const prefix = generateUniquePrefix().prefix[0];
+  const combinator = '';
+  prefixAllrules(scopedSS,prefix,combinator);
+  return {scopedSheet: scopedSS, prefix: prefix + existingPrefix};
+}
+
+
 
