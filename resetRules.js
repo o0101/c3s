ab934368bf3036e234cae20f76ed6e83ebb13aa9
Cris <22254235+crislin2046@users.noreply.github.com>
2018-09-15 11:55:01 +0000

    Added normalize CSS.

--- resetRules.js
+++ resetRules.js
@@ -1,5 +1,33 @@
 export const resetRules = [
-  `* {
-    letter-spacing: normal;
-  }`
+	`html { line-height: 1.15; text-size-adjust: 100%; }`,
+	`body { margin: 0px; }`,
+	`h1 { font-size: 2em; margin: 0.67em 0px; }`,
+	`hr { box-sizing: content-box; height: 0px; overflow: visible; }`,
+	`pre { font-family: monospace, monospace; font-size: 1em; }`,
+	`a { background-color: transparent; }`,
+	`abbr[title] { border-bottom: none; text-decoration: underline dotted; }`,
+	`b, strong { font-weight: bolder; }`,
+	`code, kbd, samp { font-family: monospace, monospace; font-size: 1em; }`,
+	`small { font-size: 80%; }`,
+	`sub, sup { font-size: 75%; line-height: 0; position: relative; vertical-align: baseline; }`,
+	`sub { bottom: -0.25em; }`,
+	`sup { top: -0.5em; }`,
+	`img { border-style: none; }`,
+	`button, input, optgroup, select, textarea { font-family: inherit; font-size: 100%; line-height: 1.15; margin: 0px; }`,
+	`button, input { overflow: visible; }`,
+	`button, select { text-transform: none; }`,
+	`button, [type="button"], [type="reset"], [type="submit"] { -webkit-appearance: button; }`,
+	`fieldset { padding: 0.35em 0.75em 0.625em; }`,
+	`legend { box-sizing: border-box; color: inherit; display: table; max-width: 100%; padding: 0px; white-space: normal; }`,
+	`progress { vertical-align: baseline; }`,
+	`textarea { overflow: auto; }`,
+	`[type="checkbox"], [type="radio"] { box-sizing: border-box; padding: 0px; }`,
+	`[type="number"]::-webkit-inner-spin-button, [type="number"]::-webkit-outer-spin-button { height: auto; }`,
+	`[type="search"] { -webkit-appearance: textfield; outline-offset: -2px; }`,
+	`[type="search"]::-webkit-search-decoration { -webkit-appearance: none; }`,
+	`::-webkit-file-upload-button { -webkit-appearance: button; font: inherit; }`,
+	`details { display: block; }`,
+	`summary { display: list-item; }`,
+	`template { display: none; }`,
+	`[hidden] { display: none; }`
 ];
