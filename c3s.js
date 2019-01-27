3be1b792188984f921e1c34904bd79927a3b9fdf
Cris <22254235+crislin2046@users.noreply.github.com>
2019-01-27 02:44:04 +0000

    Closes #4. Media queries work.

--- c3s.js
+++ c3s.js
@@ -65,86 +65,59 @@ export function cloneStyleSheet(ss) {
 }
 
 export function prefixAllRules(ss, prefix, combinator = ' ') {
-  const ruleCount = ss.cssRules.length - 1;
-  let i = ruleCount;
+  const lastRuleIndex = ss.cssRules.length - 1;
+  let i = lastRuleIndex;
 
   while(i >= 0) {
-    const lastRule = ss.cssRules[ruleCount];
-    let newRuleText = lastRule.cssText;
+    const lastRule = ss.cssRules[lastRuleIndex];
+    console.log(lastRule);
     if ( lastRule.type == CSSRule.STYLE_RULE ) {
-      const {selectorText} = lastRule;
-      const selectors = selectorText.split(/\s*,\s*/g);
-      const modifiedSelectors = selectors.map(sel => {
-        // we also need to insert prefix BEFORE any descendent combinators
-        const firstDescendentIndex = sel.indexOf(' ');
-        if ( firstDescendentIndex > -1 ) {
-          const firstSel = sel.slice(0, firstDescendentIndex);
-          const restSel = sel.slice(firstDescendentIndex);
-          // we also need to insert prefix BEFORE any pseudo selectors 
-            // NOTE: the following indexOf test will BREAK if selector contains a :
-            // such as [ns\\:name="scoped-name"]
-          const firstPseudoIndex = firstSel.indexOf(':');
-          if ( firstPseudoIndex > -1 ) {
-            const [pre, post] = [ firstSel.slice(0, firstPseudoIndex ), firstSel.slice(firstPseudoIndex) ];
-            return `${pre}${prefix}${post}${restSel}, ${prefix} ${sel}`;
-          } else return `${firstSel}${prefix}${restSel}, ${prefix} ${sel}`;
-        } else {
-          const firstPseudoIndex = sel.indexOf(':');
-          if ( firstPseudoIndex > -1 ) {
-            const [pre, post] = [ sel.slice(0, firstPseudoIndex ), sel.slice(firstPseudoIndex) ];
-            return `${pre}${prefix}${post}, ${prefix} ${sel}`;
-          } else return `${sel}${prefix}, ${prefix} ${sel}`;
-        }
-      });
-      const ruleBlock = newRuleText.slice(newRuleText.indexOf('{'));
-      const newRuleSelectorText = modifiedSelectors.join(', ');
-      newRuleText = `${newRuleSelectorText} ${ruleBlock}`;
+      prefixStyleRule(lastRule, ss, lastRuleIndex, prefix, combinator)
+    } else if ( lastRule.type == CSSRule.MEDIA_RULE ) {
+      const rules = Array.from(lastRule.cssRules);
+      const lastIndex = rules.length - 1;
+      for ( const rule of rules ) {
+        prefixStyleRule(rule, lastRule, lastIndex, prefix, combinator);
+      }
+      ss.deleteRule(lastRuleIndex);
+      ss.insertRule(lastRule.cssText, 0);
     }
-    ss.deleteRule(ruleCount);
-    ss.insertRule(newRuleText);
     i--;
   }
 }
-// The old prefix function (kept in case we need it in future)
-  // we are currently testing using both combinators
-  // to allow style sheets to be used as is
-  // whether we are scoping them to the children of the container that bears the prefix class
-  // or to the container itself
-  // since typicall style rules in a stylesheet might be written both with and without 
-  // a container prefix, and if we only use one type of combinator (direct or descendent)
-  // then rules written and intended to be usable via the other type of combinator 
-  // will not work
-  /**
-    // combinator can also be empty string ALL rules are to apply to component container
-    // but generally this is no. 
-    export function prefixAllRules(ss, prefix, combinator = ' ') {
-      const ruleCount = ss.cssRules.length - 1;
-      let i = ruleCount;
 
-      while(i >= 0) {
-        const lastRule = ss.cssRules[ruleCount];
-        let newRuleText = lastRule.cssText;
-        if ( lastRule.type == CSSRule.STYLE_RULE ) {
-          const {selectorText} = lastRule;
-          const selectors = selectorText.split(/,/g);
-          const modifiedSelectors = selectors.map(sel => {
-              if ( combinator == '' ) {
-                // an empty combinator indicates we want it to apply to this level
-                // we append the class if we want it to apply to the
-                // selection and not the parent
-                return `${sel}${prefix}`;
-              } else return `${prefix}${combinator}${sel}`;
-          });
-          const ruleBlock = newRuleText.slice(newRuleText.indexOf('{'));
-          const newRuleSelectorText = modifiedSelectors.join(', ');
-          newRuleText = `${newRuleSelectorText} ${ruleBlock}`;
-        }
-        ss.deleteRule(ruleCount);
-        ss.insertRule(newRuleText);
-        i--;
-      }
+function prefixStyleRule(lastRule, ss, lastRuleIndex, prefix, combinator) {
+  let newRuleText = lastRule.cssText;
+  const {selectorText} = lastRule;
+  const selectors = selectorText.split(/\s*,\s*/g);
+  const modifiedSelectors = selectors.map(sel => {
+    // we also need to insert prefix BEFORE any descendent combinators
+    const firstDescendentIndex = sel.indexOf(' ');
+    if ( firstDescendentIndex > -1 ) {
+      const firstSel = sel.slice(0, firstDescendentIndex);
+      const restSel = sel.slice(firstDescendentIndex);
+      // we also need to insert prefix BEFORE any pseudo selectors 
+        // NOTE: the following indexOf test will BREAK if selector contains a :
+        // such as [ns\\:name="scoped-name"]
+      const firstPseudoIndex = firstSel.indexOf(':');
+      if ( firstPseudoIndex > -1 ) {
+        const [pre, post] = [ firstSel.slice(0, firstPseudoIndex ), firstSel.slice(firstPseudoIndex) ];
+        return `${pre}${prefix}${post}${restSel}, ${prefix} ${sel}`;
+      } else return `${firstSel}${prefix}${restSel}, ${prefix} ${sel}`;
+    } else {
+      const firstPseudoIndex = sel.indexOf(':');
+      if ( firstPseudoIndex > -1 ) {
+        const [pre, post] = [ sel.slice(0, firstPseudoIndex ), sel.slice(firstPseudoIndex) ];
+        return `${pre}${prefix}${post}, ${prefix} ${sel}`;
+      } else return `${sel}${prefix}, ${prefix} ${sel}`;
     }
-  **/
+  });
+  const ruleBlock = newRuleText.slice(newRuleText.indexOf('{'));
+  const newRuleSelectorText = modifiedSelectors.join(', ');
+  newRuleText = `${newRuleSelectorText} ${ruleBlock}`;
+  ss.deleteRule(lastRuleIndex);
+  ss.insertRule(newRuleText, 0);
+}
 
 export async function scopeStyleSheet(url,prefix,combinator = ' ') {
   const ss = findStyleSheet(url);
